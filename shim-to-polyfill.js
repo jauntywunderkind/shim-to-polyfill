de5a4228a645653b8910383952769fd16fe8f1c8
jauntywunderkind <jaunty+wunder+kind+scm@eldergods.com>
2020-01-12 16:01:28 -0500

    allow fallbackModule to be an import() promise

--- shim-to-polyfill.js
+++ shim-to-polyfill.js
@@ -1,12 +1,19 @@
 "use module"
-export async function ShimToPolyfill( globalName, fallbackModuleModule){
+export async function ShimToPolyfill( globalName, fallbackModule){
 	const g= globalThis[ globalName]
 	if( g!== undefined){
 		return g
 	}
-	const
-		m= await import( fallbackModule),
-		pick= m[ globalName]|| m.default[ globalName]|| m.default
+	if( fallbackModule.then){
+		fallbackModule= await fallbackModule
+	}else{
+		fallbackModule= await import( fallbackModule)
+	}
+	const pick=
+		fallbackModule[ globalName]||
+		(fallbackModule.default&& fallbackModule.default[ globalName])||
+		fallbackModule.default
+
 	if( !pick){
 		throw new Error( "Could not find ${globalName}")
 	}
